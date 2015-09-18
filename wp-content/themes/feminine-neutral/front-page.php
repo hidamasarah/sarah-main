4a9f561b4cc5c495a4a458b1e13227be2bf62f04
Sarah E <Hidama@hidamaIII.local>
2015-09-18 11:17:42 -0500

    Added a carousel that displays posts by id tag and changed the sidebar to be full width.

---
+++ wp-content/themes/feminine-neutral/front-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The template for displaying the homepage
+ *
+ * @package WordPress
+ * @subpackage Feminine Neutral
+ * @since Feminine Neutral 1.0
+ */
+
+get_header(); ?>
+
+
+
+<?php get_footer(); ?>
